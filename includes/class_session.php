c084a77ee6b41566ec0c9c45ae3006c1ac550f8d
Kalle Sommer Nielsen <kalle@php.net>
2010-05-20 07:32:31 +0000

    Started to revamp the user API, this commit decouples the session into its own class

---
+++ includes/class_session.php
@@ -0,0 +1,174 @@
+<?php
+	/**
+	 * Tuxxedo Software Engine
+	 * =============================================================================
+	 *
+	 * @author		Kalle Sommer Nielsen <kalle@tuxxedo.net>
+	 * @version		1.0
+	 * @copyright		Tuxxedo Software Development 2006+
+	 * @package		Engine
+	 *
+	 * =============================================================================
+	 */
+
+	defined('TUXXEDO') or exit;
+
+
+	/**
+	 * Session interface, this class is designed to be attached to 
+	 * classes that implements an interface based on sessions.
+	 *
+	 * @author		Kalle Sommer Nielsen <kalle@tuxxedo.net>
+	 * @version		1.0
+	 * @package		Engine
+	 */
+	abstract class Tuxxedo_Session extends Tuxxedo_InfoAccess
+	{
+		/**
+		 * Whether a session is started or not
+		 *
+		 * @var		boolean
+		 */
+		protected static $started	= false;
+
+		/**
+		 * The session options, such as prefix, path etc.
+		 *
+		 * @var		array
+		 */
+		protected static $options	= Array(
+							'expires'	=> 1800, 
+							'prefix'	=> '', 
+							'domain'	=> '', 
+							'path'		=> ''
+							);
+
+
+		/**
+		 * Magic method called when creating a new instance of the 
+		 * object from the registry
+		 *
+		 * @param	Tuxxedo			The Tuxxedo object reference
+		 * @param	array			The configuration array
+		 * @param	array			The options array
+		 * @return	object			Object instance
+		 */
+		final public static function invoke(Tuxxedo $tuxxedo, Array $configuration = NULL, Array $options = NULL)
+		{
+			self::$started = true;
+			self::$options = Array(
+						'expires'	=> $options['cookie_expires'], 
+						'prefix'	=> $options['cookie_prefix'], 
+						'domain'	=> $options['cookie_domain'], 
+						'path'		=> $options['cookie_path']
+						);
+
+			session_set_cookie_params($options['cookie_expires'], $options['cookie_domain'], $options['cookie_path'], false, true);
+			session_start();
+		}
+
+		/**
+		 * Gets a session variable
+		 *
+		 * @param	string			Variable name
+		 * @param	boolean			Whether to include the session prefix or not, defaults to true
+		 * @return	mixed			Returns the session variable value on success, or null on failure
+		 */
+		final public static function get($name, $prefix = true)
+		{
+			if($prefix)
+			{
+				$name = self::$options['prefix'] . $name;
+			}
+
+			if(!isset($_SESSION[$name]))
+			{
+				return(NULL);
+			}
+
+			return($_SESSION[$name]);
+		}
+
+		/**
+		 * Sets a session variable
+		 *
+		 * @param	string			Variable name
+		 * @param	mixed			Variable value
+		 * @param	boolean			Whether to include the session prefix or not, defaults to true
+		 * @return	void			No value is returned
+		 */
+		final public static function set($name, $value, $prefix = true)
+		{
+			if($prefix)
+			{
+				$name = self::$options['prefix'] . $name;
+			}
+
+			$_SESSION[$name] = $value;
+		}
+
+		/**
+		 * Terminates a session
+		 *
+		 * @return	void			No value is returned
+		 */
+		final public static function terminate()
+		{
+			session_unset();
+			session_destroy();
+
+			self::$started = false;
+		}
+
+		/**
+		 * Checks whether a session variable is available 
+		 *
+		 * @param	scalar			The information row name to check
+		 * @return	boolean			Returns true if the information is stored, otherwise false
+		 */
+		public function offsetExists($offset)
+		{
+			return(isset($_SESSION[self::$options['prefix'] . $offset]));
+		}
+
+		/**
+		 * Gets a value from a session variable
+		 * 
+		 * @param	scalar			The information row name to get
+		 * @return	mixed			Returns the information value, and NULL if the value wasn't found
+		 */
+		public function offsetGet($offset)
+		{
+			if(isset($_SESSION[self::$options['prefix'] . $offset]))
+			{
+				return($_SESSION[self::$options['prefix'] . $offset]);
+			}
+		}
+
+		/**
+		 * Sets a new session variable
+		 *
+		 * @param	scalar			The information row name to set
+		 * @param	mixed			The new/update value for this row
+		 * @return	void			No value is returned
+		 */
+		public function offsetSet($offset, $value)
+		{
+			$_SESSION[self::$options['prefix'] . $offset] = $value;
+		}
+
+		/**
+		 * Deletes a session variable
+		 *
+		 * @param	scalar			The information row name to delete
+		 * @return	void			No value is returned
+		 */
+		public function offsetUnset($offset)
+		{
+			if(isset($_SESSION[self::$options['prefix'] . $offset]))
+			{
+				unset($_SESSION[self::$options['prefix'] . $offset]);
+			}
+		}
+	}
+?>
