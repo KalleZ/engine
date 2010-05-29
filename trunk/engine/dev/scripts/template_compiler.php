0771e65f311f483ba70238336b89f6e82ad921db
kalle.php <kalle.php@cd68e5fc-efee-11de-91ba-91c70a792394>
2010-05-29 16:55:17 +0000

    New basic script to compile a template without using the actual style & templates tool

---
+++ trunk/engine/dev/scripts/template_compiler.php
@@ -0,0 +1,47 @@
+<?php
+	define('TUXXEDO', 1337);
+	require('../../includes/class_core.php');
+	require('../../includes/class_filter.php');
+	require('../../includes/class_template_compiler.php');
+	require('../../includes/functions.php');
+
+	$filter	= new Tuxxedo_Filter;
+
+	if(isset($_POST['progress']))
+	{
+		$compiler = new Tuxxedo_Template_Compiler;
+		$compiler->set($filter->post('sourcecode'));
+?>
+<h2>Compile result</h2>
+<?php
+
+		if($compiler->test() !== true)
+		{
+			try
+			{
+				$compiler->compile();
+			}
+			catch(Tuxxedo_Template_Compiler_Exception $e)
+			{
+?>
+<p><?php echo($e->getMessage()); ?></p>
+<?php
+			}
+		}
+		else
+		{
+			$compiler->compile();
+?>
+<textarea name="sourcecode" cols="100" rows="15"><?php echo(htmlspecialchars($compiler->get())); ?></textarea>
+<?php
+		}
+	}
+?>
+<form action="./template_compiler.php" method="POST">
+	<h2>Template compiler</h2>
+
+	<textarea name="sourcecode" cols="100" rows="15"><?php echo(htmlspecialchars($filter->post('sourcecode'))); ?></textarea>
+
+	<p />
+	<input type="submit" name="progress" value="Compile" />
+</form>
