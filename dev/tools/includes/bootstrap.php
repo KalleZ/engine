ba39bb7847895b31e7f932e8ed83058ef8705f0c
Kalle Sommer Nielsen <kalle@php.net>
2010-06-02 05:44:13 +0000

    * Finished implementing the datastore developmental tool * Added error and redirect templates * Fixed minor bugs here and there

--- dev/tools/includes/bootstrap.php
+++ dev/tools/includes/bootstrap.php
@@ -38,10 +38,20 @@
 
 	if(!defined('SCRIPT_NAME'))
 	{
-		// HACK HACK HACK
-		define('SCRIPT_NAME', 'void');
+		throw new Tuxxedo_Basic_Exception('A script name must be defined prior to use');
+	}
 
-		//throw new Tuxxedo_Basic_Exception('A script name must be defined prior to use');
+	if(defined('PHP_VERSION_ID'))
+	{
+		define('TUXXEDO_PHP_VERSION', PHP_VERSION_ID);
+	}
+	else
+	{
+		$version = PHP_VERSION;
+
+		define('TUXXEDO_PHP_VERSION', ($version{0} * 10000 + $version{2} * 100 + $version{4}));
+
+		unset($version);
 	}
 
 	Tuxxedo::globals('error_reporting', 	true);
@@ -49,12 +59,12 @@
 
 	set_error_handler('tuxxedo_error_handler');
 	set_exception_handler('tuxxedo_exception_handler');
+	register_shutdown_function('tuxxedo_shutdown_handler');
 	spl_autoload_register('tuxxedo_autoload_handler');
 
 	define('TUXXEDO_DEBUG', 	true);
 	define('TUXXEDO_DIR', 		CWD);
 	define('TUXXEDO_PREFIX', 	$configuration['database']['prefix']);
-	define('TUXXEDO_PHP_VERSION', 	PHP_VERSION_ID);
 
 	require('./includes/class_template.php');
 
@@ -70,7 +80,7 @@
 	define('TIMENOW', $datetime->getTimestamp());
 
 	$cache_buffer		= Array();
-	$default_templates 	= Array('header', 'footer');
+	$default_templates 	= Array('header', 'footer', 'error', 'redirect');
 
 	if(isset($action_templates) && isset($_REQUEST['do']) && array_key_exists((string) $_REQUEST['do'], $action_templates))
 	{
@@ -80,6 +90,8 @@
 	$style->cache((!isset($templates) ? $default_templates : array_merge($default_templates, (array) $templates)), $cache_buffer) or tuxxedo_multi_error('Unable to load template \'%s\'', $cache_buffer);
 
 	unset($cache_buffer);
+
+	$engine_version = Tuxxedo::VERSION_STRING;
 
 	eval('$header = "' . $style->fetch('header') . '";');
 	eval('$footer = "' . $style->fetch('footer') . '";');
