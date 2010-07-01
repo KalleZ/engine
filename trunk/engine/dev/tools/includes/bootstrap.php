18aff7c9ca025ec369f5a09fcc5e89b10037cc51
kalle.php <kalle.php@cd68e5fc-efee-11de-91ba-91c70a792394>
2010-07-01 02:42:39 +0000

    Implemented the new autoloader class within the core, allowing custom mappings to take place

--- trunk/engine/dev/tools/includes/bootstrap.php
+++ trunk/engine/dev/tools/includes/bootstrap.php
@@ -60,7 +60,7 @@
 	set_error_handler('tuxxedo_error_handler');
 	set_exception_handler('tuxxedo_exception_handler');
 	register_shutdown_function('tuxxedo_shutdown_handler');
-	spl_autoload_register('tuxxedo_autoload_handler');
+	spl_autoload_register(Array('Tuxxedo_Autoloader', 'load'));
 
 	define('TUXXEDO_DEBUG', 	true);
 	define('TUXXEDO_DIR', 		CWD);
