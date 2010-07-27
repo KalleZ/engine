c691d899245bce7c63a873bfdfcd0fb12891f420
kalle.php <kalle.php@cd68e5fc-efee-11de-91ba-91c70a792394>
2010-07-27 07:14:52 +0000

    Instead of placing all exceptions within \Tuxxedo\Core as originally planned, then each component \Tuxxedo\<component>\ must define its own within that component namespace in the form:  * \Tuxxedo\<component>\Exceptions  * \Tuxxedo\<component>\...\Exceptions
    Als add the empty handlers class for now

---
+++ trunk/engine/library/tuxxedo/core/handlers.php
@@ -0,0 +1,29 @@
+<?php
+	/**
+	 * Tuxxedo Software Engine
+	 * =============================================================================
+	 *
+	 * @author		Kalle Sommer Nielsen 	<kalle@tuxxedo.net>
+	 * @author		Ross Masters 		<ross@tuxxedo.net>
+	 * @version		1.0
+	 * @copyright		Tuxxedo Software Development 2006+
+	 * @package		Engine
+	 * @subpackage		Core
+	 *
+	 * =============================================================================
+	 */
+
+
+	/**
+	 * Core engine namespace, standard exceptions are integrated within this 
+	 * part of the namespace, functions that previously were procedural is 
+	 * defined as static classes.
+	 *
+	 * @author		Kalle Sommer Nielsen	<kalle@tuxxedo.net>
+	 * @author		Ross Masters 		<ross@tuxxedo.net>
+	 * @version		1.0
+	 * @package		Engine
+	 * @subpackage		Core
+	 */
+	namespace \Tuxxedo\Core;
+?>
