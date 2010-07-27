0ae749d9ea3ed0134020d36358980a6cc7670759
Kalle Sommer Nielsen <kalle@php.net>
2010-07-27 00:00:26 +0000

    Began to re-implement the core code into namespaces, still needs the functions.php in the core namespace:  * The registry class is now \Tuxxedo\Core\Registry  * The registry no longer have "components", meaning the load and component methods are now gone  * The registry no longer depends on the cached options and the invokable interface (\Tuxxedo\Core\Interfaces\Invokable) does not pass the 3rd option parameter anymore  * The registry will not try to look for the configuration array within the global scope if none have been set

---
+++ library/tuxxedo/core/interfaces/invokable.php
@@ -0,0 +1,53 @@
+<?php
+	/**
+	 * Tuxxedo Software Engine
+	 * =============================================================================
+	 *
+	 * @author		Kalle Sommer Nielsen 	<kalle@tuxxedo.net>
+	 * @author		Ross Masters 		<ross@tuxxedo.net>
+	 * @version		1.0
+	 * @copyright		Tuxxedo Software Development 2006+
+	 * @package		Engine
+	 * @subpackage		Core
+	 *
+	 * =============================================================================
+	 */
+
+
+	/**
+	 * Core engine namespace, the only dependency from the core is the 
+	 * exceptions namespace.
+	 *
+	 * @author		Kalle Sommer Nielsen	<kalle@tuxxedo.net>
+	 * @author		Ross Masters 		<ross@tuxxedo.net>
+	 * @version		1.0
+	 * @package		Engine
+	 * @subpackage		Core
+	 */
+	namespace \Tuxxedo\Core\Interfaces;
+
+
+	/**
+	 * Interface for requring the registry to pass certain information 
+	 * before the constructor is called.
+	 *
+	 * @author		Kalle Sommer Nielsen <kalle@tuxxedo.net>
+	 * @version		1.0
+	 * @package		Engine
+	 */
+	interface Invokable
+	{
+		/**
+		 * Magic method called when creating a new instance of the 
+		 * object from the registry
+		 *
+		 * @param	\Tuxxedo\Core\Registry			The registry object reference
+		 * @param	array					The configuration array
+		 * @param	array					The options array
+		 * @return	object					Object instance
+		 *
+		 * @throws	Tuxxedo_Basic_Exception	Only thrown on poorly a configured database section in the configuration file
+		 */
+		public static function invoke(\Tuxxedo\Core\Registry $registry, Array $configuration);
+	}
+?>
