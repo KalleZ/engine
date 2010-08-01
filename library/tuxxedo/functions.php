8c64408179f4acfc899e82fc886d598f2cd79ee5
Kalle Sommer Nielsen <kalle@php.net>
2010-08-01 05:27:10 +0000

    * Added versioning class for engine versioning * Functions are no longer a static class and should be in its own functions files in the "global" namespace

--- library/tuxxedo/functions.php
+++ library/tuxxedo/functions.php
@@ -12,18 +12,4 @@
 	 *
 	 * =============================================================================
 	 */
-
-
-	/**
-	 * Core engine namespace, standard exceptions are integrated within this 
-	 * part of the namespace, functions that previously were procedural is 
-	 * defined as static classes.
-	 *
-	 * @author		Kalle Sommer Nielsen	<kalle@tuxxedo.net>
-	 * @author		Ross Masters 		<ross@tuxxedo.net>
-	 * @version		1.0
-	 * @package		Engine
-	 * @subpackage		Core
-	 */
-	namespace \Tuxxedo\Core;
 ?>
