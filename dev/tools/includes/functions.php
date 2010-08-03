4a13c4ff9264f5e874cece414f9f3905ff869fb8
Kalle Sommer Nielsen <kalle@php.net>
2010-08-03 08:14:44 +0000

    Started to implement the "Tools" menu in the DevTools, currently only the stat script is implemented

--- dev/tools/includes/functions.php
+++ dev/tools/includes/functions.php
@@ -58,4 +58,49 @@
 
 		return((string) $value);
 	}
+
+	/**
+	 * A recursive glob function
+	 *
+	 * @param	string			The glob expression to execute
+	 * @return	array			Returns an array containing the matched elements and false on error
+	 */
+	function recursive_glob($expression)
+	{
+		static $expression_prefix;
+
+		if(!$expression_prefix)
+		{
+			$expression_prefix = strlen($expression) + 1;
+		}
+
+		$glob = glob($expression . '/*');
+
+		if(!sizeof($glob))
+		{
+			return(false);
+		}
+
+		$return_value = Array();
+
+		foreach($glob as $entry)
+		{
+			if(is_dir($entry))
+			{
+				if(($entries = recursive_glob($entry)) !== false)
+				{
+					foreach($entries as $sub_entry)
+					{
+						$return_value[] = $sub_entry;
+					}
+				}
+
+				continue;
+			}
+
+			$return_value[] = str_replace('\\', '/', substr_replace($entry, '', 0, $expression_prefix));
+		}
+
+		return($return_value);
+	}
 ?>
