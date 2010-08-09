aca342b711b05bfec93fd13932dd9b25ebb20bfe
kalle.php <kalle.php@cd68e5fc-efee-11de-91ba-91c70a792394>
2010-08-09 22:21:32 +0000

    Many changes to the controller interface, mostly minor design changes to let it flow better with other parts of the Engine.
    Ross, please review this change and see if I broke some of your applications here

--- trunk/engine/library/controller.php
+++ trunk/engine/library/controller.php
@@ -14,163 +14,171 @@
 
 	defined('TUXXEDO') or exit;
 
+
 	/**
-	 * The base controller class for Tuxxedo's MVC components
+	 * Interface for dispatchable controller hooks
+	 *
+	 * @author		Kalle Sommer Nielsen	<kalle@tuxxedo.net>
+	 * @author		Ross Masters 		<ross@tuxxedo.net>
+	 * @version		1.0
 	 * @package		Engine
-	 * @subpackage	MVC
+	 * @subpackage		MVC
 	 */
-	class Tuxxedo_Controller
+	interface Tuxxedo_Controller_Dispatchable
 	{
 		/**
+		 * Dispatch hook constant - Pre dispatching
+		 *
+		 * @var		integer
+		 */
+		const DISPATCH_PRE		= 1;
+
+		/**
+		 * Dispatch hook constant - Post dispatching
+		 *
+		 * @var		integer
+		 */
+		const DISPATCH_POST		= 2;
+
+
+		/**
+		 * Controller dispatch hook, this hook is called for 
+		 * both pre and post dispatching and uses its only 
+		 * parameter to determine which state we currently are 
+		 * in
+		 *
+		 * @param	integer				The current dispatcher state
+		 * @return	void				No value is returned
+		 */
+		public function dispatcher($mode);
+	}
+
+	/**
+	 * The base controller class for the MVC components
+	 *
+	 * @author		Kalle Sommer Nielsen	<kalle@tuxxedo.net>
+	 * @author		Ross Masters 		<ross@tuxxedo.net>
+	 * @version		1.0
+	 * @package		Engine
+	 * @subpackage		MVC
+	 */
+	abstract class Tuxxedo_Controller
+	{
+		/**
+		 * Private instance to the Tuxxedo registry
+		 *
+		 * @var		Tuxxedo
+		 */
+		protected $tuxxedo;
+
+		/**
 		 * HTTP request object
-		 * @var		Tuxxedo_Request
+		 *
+		 * @var		Tuxxedo_Request_HTTP
 		 */
 		protected $request;
-		
+
 		/**
 		 * Router object
+		 *
 		 * @var		Tuxxedo_Router
 		 */
 		protected $router;
-		
+
 		/**
 		 * Layout template object
+		 *
 		 * @var		Tuxxedo_Template
 		 */
 		protected $layout;
-		
+
 		/**
 		 * Current view template
+		 *
 		 * @var		Tuxxedo_Template
 		 */
 		protected $view;
-	
+
+
+		/**
+		 * Constructor
+		 *
+		 * @param	Tuxxedo			The Tuxxedo object reference
+		 */
+		public function __construct(Tuxxedo $tuxxedo)
+		{
+			$this->tuxxedo = $tuxxedo;
+		}
+
 		/**
 		 * Set the request object
 		 * @param	Tuxxedo_Request		Request object
 		 */
-		public function setRequest(Tuxxedo_Request $request) 
+		final public function setRequest(Tuxxedo_Request $request) 
 		{
 			$this->request = $request;
 		}
-	
-		/**
-		 * Get the request object (externally)
-		 * @return		Tuxxedo_Request
-		 */
-		public function getRequest()
-		{
-			return $this->request;
-		}
-	
+
 		/**
 		 * Set the router object
 		 * @param	Tuxxedo_Router		Router object used
 		 */
-		public function setRouter(Tuxxedo_Router $router)
+		final public function setRouter(Tuxxedo_Router $router)
 		{
 			$this->router = $router;
 		}
-		
-		/**
- 		 * Get the router object externally
- 		 * @return		Tuxxedo_Router
-		 */
-		public function getRouter()
-		{
-			return $this->router;
-		}
-		
+
 		/**
 		 * Set the template object used for the layout
 		 * @param		Tuxxedo_Template	Layout template
 		 */
-		public function setLayout(Tuxxedo_View $layout)
+		final public function setLayout(Tuxxedo_View $layout)
 		{
 			$this->layout = $layout;
 		}
-	
-		/**
-		 * Get the layout object externally
-		 * @return		Tuxxedo_Template	Layout template
-		 */
-		public function getLayout()
-		{
-			return $this->layout;
-		}
-	
+
 		/**
 		 * Set the view object
 		 * @param		Tuxxedo_Template	View template for the current action
 		 */
-		public function setView(Tuxxedo_View $view)
+		final public function setView(Tuxxedo_View $view)
 		{
 			$this->view = $view;
 		}
-	
+
 		/**
-		 * Get the view object externally
-		 * @return		Tuxxedo_Template	View template
+		 * Dispatches the controller and renders the page content
+		 *
+		 * @return		string			Rendered view
+		 *
+		 * @throws		Tuxxedo_Basic_Exception	If the controller does not
 		 */
-		public function getView()
+		final public function dispatch()
 		{
-			return $this->view;
-		}
-	
-		/**
-		 * Init hook for controllers to run code before the controller's action
-		 * is dispatched. The first method called in the controller's dispatch
-		 * routine. Generally used for instantiating common variables.
-		 * @return		void
-		 */
-		public function init() { }
-	
-		/**
-		 * Hook for the controller to execute code before it is dispatched.
-		 * Called after init and generally used for checking authentication - 
-		 * e.g. whether the user is authenticated and has permissions.
-		 * @return		void
-		 */
-		public function preDispatch() { }
-		
-		/**
-		 * Hook for the controller to execute code after it is dispatched.
-		 * The last method called during the dispatch before the view is
-		 * returned. Generally used for any last minute view modifications.
-		 * @return		void
-		 */
-		public function postDispatch() { }
-	
-		/**
-		 * Dispatch the controller, returning a rendered View object.
-		 * Calls a series of hooks in the order: {@link init}, 
-		 * {@link preDispatch}, action and {@link postDispatch}.
-		 * @throws		Tuxxedo_Basic_Exception		If the controller does not
-		 * 											define the action.
-		 * @return		Tuxxedo_View	Rendered view
-		 */
-		public function dispatch()
-		{
-			// Execute any initialisation logic the controller needs
-			$this->init();
-			
-			// Call pre-dispatch hook method
-			$this->preDispatch();
-		
-			// Get the action name and call its method
-			$method = $this->router->getAction() . "Action";
-		
-			// Check the method exists first
-			if (!method_exists($this, $method))
+			if($this instanceof Tuxxedo_Controller_Dispatchable)
 			{
-				$className = get_class($this);
-				throw new Tuxxedo_Basic_Exception(
-					"Action method does not exist in $className.");
+				$this->dispatcher(self::DISPATCH_PRE);
 			}
-		
+
+			$action = $this->router->getAction() . 'Action';
+
+			if(!method_exists($this, $method))
+			{
+				throw new Tuxxedo_Exception('Unknown action called');
+			}
+
 			$this->$method();
-		
-			// Call the post-dispatch hook
-			$this->postDispatch();
+
+			if($this instanceof Tuxxedo_Controller_Dispatchable)
+			{
+				$this->dispatcher(self::DISPATCH_POST);
+			}
+
+			/**
+		 	 * @TODO	This will change once the Tuxxedo_View class is implemented
+			 */
+			eval('$view = "' . $this->tuxxedo->style->fetch($this->view) . '";');
+			eval('return("' . $this->layout . ");');
 		}
 	}
+?>
