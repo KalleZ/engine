af552383fc80c037a434ecbfe0756d37845fbf1c
Kalle Sommer Nielsen <kalle@php.net>
2010-08-12 22:24:31 +0000

    Added docblock comments

--- dev/tools/includes/test.php
+++ dev/tools/includes/test.php
@@ -27,32 +27,87 @@
 	 */
 	class Tuxxedo_Test
 	{
+		/**
+		 * Option constant - test is required
+		 *
+		 * @var		integer
+		 */
 		const OPT_REQUIRED	= 1;
+
+		/**
+		 * Option constant - test is optional
+		 *
+		 * @var		integer
+		 */
 		const OPT_OPTIONAL	= 2;
+
+		/**
+		 * Option constant - tests a PHP extension
+		 *
+		 * @var		integer
+		 */
 		const OPT_EXTENSION	= 4;
+
+		/**
+		 * Option constant - tests versioning
+		 *
+		 * @var		integer
+		 */
 		const OPT_VERSION	= 8;
 
 
+		/**
+		 * Option bitfield
+		 *
+		 * @var		integer
+		 */
 		protected $options	= 0;
+
+		/**
+		 * Testing arguments
+		 *
+		 * @var		array
+		 */
 		protected $argv		= Array();
 
 
-		public function __construct($options, Array $argv = Array())
+		/**
+		 * Constructs a new test case
+		 *
+		 * @param	integer			The options bitfield, using the OPT_* class constants
+		 * @param	array			The arguments that needs to be tested against
+		 */
+		public function __construct($options, Array $argv)
 		{
 			$this->options 	= (integer) $options;
 			$this->argv 	= $argv;
 		}
 
+		/**
+		 * Check if a test is required
+		 *
+		 * @return	boolean			Returns true if the test is required otherwise false
+		 */
 		public function isRequired()
 		{
 			return(($this->options & self::OPT_REQUIRED) > 0);
 		}
 
+		/**
+		 * Check if a test is optional
+		 *
+		 * @return	boolean			Returns true if the test is optional otherwise false
+		 */
 		public function isOptional()
 		{
 			return(($this->options & self::OPT_OPTIONAL) > 0);
 		}
 
+		/**
+		 * Executes the test case
+		 *
+		 * @return	boolean			Returns true if the test passed, otherwise false
+		 */
 		public function test()
 		{
 			if($this->options & self::OPT_EXTENSION)
