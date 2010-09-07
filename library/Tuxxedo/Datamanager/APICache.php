698a068afb1b74900c0d2ca1f1feb95591e3f4dd
Kalle Sommer Nielsen <kalle@php.net>
2010-09-07 22:58:06 +0000

    Docblock for the APICache interface plugin for datamanagers

--- library/Tuxxedo/Datamanager/APICache.php
+++ library/Tuxxedo/Datamanager/APICache.php
@@ -9,11 +9,26 @@
 	 * @copyright		Tuxxedo Software Development 2006+
 	 * @license		Apache License, Version 2.0
 	 * @package		Engine
+	 * @subpackage		Library
 	 *
 	 * =============================================================================
 	 */
 
+
+	/**
+	 * Datamanager namespace, this contains all base adapter class that 
+	 * datamanagers must extend in order to become loadable. The root 
+	 * namespace also hosts interfaces that datamanagers can implement 
+	 * to extend the magic within.
+	 *
+	 * @author		Kalle Sommer Nielsen	<kalle@tuxxedo.net>
+	 * @author		Ross Masters 		<ross@tuxxedo.net>
+	 * @version		1.0
+	 * @package		Engine
+	 * @subpackage		Library
+	 */
 	namespace Tuxxedo\Datamanager;
+
 
 	/**
 	 * Datastore requirement for using the datamanager
@@ -24,6 +39,7 @@
 	 * @author		Kalle Sommer Nielsen <kalle@tuxxedo.net>
 	 * @version		1.0
 	 * @package		Engine
+	 * @subpackage		Library
 	 */
 	interface APICache
 	{
@@ -31,9 +47,9 @@
 		 * This event method is called if the query to store the 
 		 * data was success, to rebuild the datastore cache
 		 *
-		 * @param	Tuxxedo			The Tuxxedo object reference
-		 * @param	array			A virtually populated array from the datamanager abstraction
-		 * @return	boolean			Returns true if the datastore was updated with success, otherwise false
+		 * @param	\Tuxxedo\Registry		The Registry reference
+		 * @param	array				A virtually populated array from the datamanager abstraction
+		 * @return	boolean				Returns true if the datastore was updated with success, otherwise false
 		 */
 		public function rebuild(Registry $registry, Array $virtual);
 	}
