0a67c5aab5cb55926cab5fbeaf2e708371c91956
Kalle Sommer Nielsen <kalle@php.net>
2010-09-08 14:08:51 +0000

    Fixed WS and docblocks in the Controller, and added a new Core exception that indicates a development limitation

---
+++ library/Tuxxedo/Exception/Core.php
@@ -0,0 +1,42 @@
+<?php
+	/**
+	 * Tuxxedo Software Engine
+	 * =============================================================================
+	 *
+	 * @author		Kalle Sommer Nielsen 	<kalle@tuxxedo.net>
+	 * @author		Ross Masters 		<ross@tuxxedo.net>
+	 * @version		1.0
+	 * @copyright		Tuxxedo Software Development 2006+
+	 * @license		Apache License, Version 2.0
+	 * @package		Engine
+	 *
+	 * =============================================================================
+	 */
+
+
+	/**
+	 * Exception namespace, this contains all the core exceptions defined within 
+	 * the library.
+	 *
+	 * @author		Kalle Sommer Nielsen	<kalle@tuxxedo.net>
+	 * @author		Ross Masters 		<ross@tuxxedo.net>
+	 * @version		1.0
+	 * @package		Engine
+	 * @subpackage		Library
+	 */
+	namespace Tuxxedo\Exception;
+
+    
+	/**
+	 * Core exceptions indicates a critical fault in the Engine, these 
+	 * are primary used during development processes and will not 
+	 * appear in production ready versions.
+	 *
+	 * @author		Kalle Sommer Nielsen <kalle@tuxxedo.net>
+	 * @version		1.0
+	 * @package		Engine
+	 */
+	class Core extends \Tuxxedo\Exception\Basic
+	{
+	}    
+?>
