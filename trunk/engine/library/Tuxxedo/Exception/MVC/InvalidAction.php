88adf7329a0e5c4be02abfa6052875bcf0508a1f
kalle.php <kalle.php@cd68e5fc-efee-11de-91ba-91c70a792394>
2010-09-09 15:10:44 +0000

    Added two new exceptions for the MVC, and got rid of one of the two Core exceptions we throw

---
+++ trunk/engine/library/Tuxxedo/Exception/MVC/InvalidAction.php
@@ -0,0 +1,53 @@
+<?php
+	/**
+	 * Tuxxedo Software Engine
+	 * =============================================================================
+	 *
+	 * @author		Kalle Sommer Nielsen 	<kalle@tuxxedo.net>
+	 * @author		Ross Masters 		<ross@tuxxedo.net>
+	 * @version		1.0
+	 * @copyright		Tuxxedo Software Development 2006+
+	 * @license		Apache License, Version 2.0
+	 * @package		Engine
+	 *
+	 * =============================================================================
+	 */
+
+
+	/**
+	 * MVC Exception namespace. This contains all the specialized exceptions for 
+	 * MVC components.
+	 *
+	 * @author		Kalle Sommer Nielsen	<kalle@tuxxedo.net>
+	 * @author		Ross Masters 		<ross@tuxxedo.net>
+	 * @version		1.0
+	 * @package		Engine
+	 * @subpackage		Library
+	 */
+	namespace Tuxxedo\Exception;
+
+
+	/**
+	 * Aliasing rules
+	 */
+	use Tuxxedo\Exception;
+
+    
+	/**
+	 * Invalid action method
+	 *
+	 * @author		Kalle Sommer Nielsen <kalle@tuxxedo.net>
+	 * @version		1.0
+	 * @package		Engine
+	 */
+	class InvalidAction extends Exception\Basic
+	{
+		/**
+		 * Constructor, sets the standardized exception message
+		 */
+		public function __construct()
+		{
+			parent::__construct('Invalid action');
+		}
+	}    
+?>
