87bfa327df6f5d3a3a29693b5cc4ae61d302db09
Kalle Sommer Nielsen <kalle@php.net>
2010-09-09 13:47:01 +0000

    Created a minimized (simple) version of the bootstraper that doesn't load any additional components

--- dev/extra/bootstrap_simple.php
+++ dev/extra/bootstrap_simple.php
@@ -119,7 +119,6 @@
 	 */
 	use Tuxxedo\Exception;
 	use Tuxxedo\Registry;
-	use Tuxxedo\User;
 
 	/**
 	 * Construct the main registry
@@ -139,110 +138,18 @@
 	define('TIMENOW_UTC', isset($_SERVER['REQUEST_TIME']) ? $_SERVER['REQUEST_TIME'] : time());
 
 	/**
-	 * Register the default instances
+	 * Set the configuration
 	 */
-	$registry->register('db', '\Tuxxedo\Database');
-	$registry->register('cache', '\Tuxxedo\Datastore');
-	/**
-	 * Precache elements from datastore
-	 */
-	$cache_buffer		= Array();
-	$default_precache 	= Array('options', 'styleinfo', 'usergroups', 'languages', 'phrasegroups');
-
-	$cache->cache((!isset($precache) ? $default_precache : array_merge($default_precache, (array) $precache)), $cache_buffer) or tuxxedo_multi_error('Unable to load datastore element \'%s\', datastore possibly corrupted', $cache_buffer);
-
-	/**
-	 * Now the datastore is loaded we must instanciate the 
-	 * user session, note that the invoke method sets the 
-	 * cookie parameters and starts session itself here
-	 */
-	$registry->register('user', 'Tuxxedo\User');
-
-	/**
-	 * Options and configuration references
-	 */
-	$registry->set('options', (object) $cache->options);
 	$registry->set('configuration', $configuration);
 
 	/**
-	 * User information references
+	 * Datetime instances
 	 */
-	$registry->set('userinfo', $user->getUserInfo(NULL, NULL, \Tuxxedo\User::OPT_CURRENT_ONLY));
-	$registry->set('usergroup', $user->getUserGroupInfo());
-
-	/**
-	 * Date and Timezone references
-	 */
-	$tz = strtoupper(empty($userinfo->id) ? $options->date_timezone : $userinfo->timezone);
-
-	if($tz != 'UTC')
-	{
-		date_default_timezone_set($tz);
-	}
-
-	$registry->set('timezone', new DateTimeZone($tz));
+	$registry->set('timezone', new DateTimeZone('UTC'));
 	$registry->set('datetime', new DateTime('now', $timezone));
-
-	unset($tz);
 
 	/**
 	 * Current time constant
 	 */
 	define('TIMENOW', $datetime->getTimestamp());
-
-	/**
-	 * We can only load the styling & internationalization APIs 
-	 * once the datastore elements are loaded and user sessions 
-	 * have been instanciated
-	 */
-	$registry->register('style', '\Tuxxedo\Style');
-	$registry->register('intl', '\Tuxxedo\Intl');
-
-	/**
-	 * Precache templates
-	 */
-	$cache_buffer		= Array();
-	$default_templates 	= Array(
-					/* Common templates */
-					'header', 'footer', 
-
-					/* Error handling */
-					'error', 
-
-					/* Miscellaneous */
-					'redirect'
-					);
-
-	if(isset($action_templates) && isset($_REQUEST['do']) && isset($action_templates[(string) $_REQUEST['do']]))
-	{
-		$default_templates = array_merge($default_templates, (array) $action_templates[(string) $_REQUEST['do']]);
-	}
-
-	$style->cache((!isset($templates) ? $default_templates : array_merge($default_templates, (array) $templates)), $cache_buffer) or tuxxedo_multi_error('Unable to load template \'%s\'', $cache_buffer);
-
-	unset($cache_buffer);
-
-	/**
-	 * Precache phrase groups
-	 */
-	$cache_buffer		= Array();
-	$default_phrasegroups	= Array(
-					/* Common groups */
-					'global'
-					);
-
-	$intl->cache((!isset($phrasegroups) ? $default_phrasegroups : array_merge($default_phrasegroups, (array) $phrasegroups)), $cache_buffer) or tuxxedo_multi_error('Unable to load phrase groups \'%s\'', $cache_buffer);
-
-	unset($cache_buffer);
-
-	/**
-	 * Get phrases
-	 */
-	$registry->set('phrase', $intl->getPhrases());
-
-	/**
-	 * Header and footer templates for the main site
-	 */
-	eval('$header = "' . $style->fetch('header') . '";');
-	eval('$footer = "' . $style->fetch('footer') . '";');
 ?>
