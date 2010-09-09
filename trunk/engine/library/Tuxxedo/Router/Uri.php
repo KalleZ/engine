44e3d328c3bbdadcd96478672b349b16253aef88
kalle.php <kalle.php@cd68e5fc-efee-11de-91ba-91c70a792394>
2010-09-09 13:31:18 +0000

    Docblock and WS for the router components

--- trunk/engine/library/Tuxxedo/Router/Uri.php
+++ trunk/engine/library/Tuxxedo/Router/Uri.php
@@ -12,84 +12,107 @@
 	 *
 	 * =============================================================================
 	 */
-	
-	namespace Tuxxedo\Router;
-	
+
+
 	/**
-	 * URI-based router
-	 * Attempts to detect routing information from a uri input
+	 * Router handler namespaces, this namespace is for various interfaces 
+	 * for hitting the router.
+	 *
+	 * @author		Kalle Sommer Nielsen	<kalle@tuxxedo.net>
+	 * @author		Ross Masters 		<ross@tuxxedo.net>
+	 * @version		1.0
+	 * @package		Engine
+	 * @subpackage		Library
+	 */
+	namespace Tuxxedo\Router;
+
+
+	/**
+	 * Uri based router interface.
+	 *
+	 * @author		Kalle Sommer Nielsen	<kalle@tuxxedo.net>
+	 * @author		Ross Masters 		<ross@tuxxedo.net>
+	 * @version		1.0
+	 * @package		Engine
+	 * @subpackage		Library
 	 */
 	class Uri extends \Tuxxedo\Router
 	{
 		/**
-		 * Parse the router information from a URI string
-		 * @param	string	Input URI
+		 * Parses a uri and dispatches them all into controller, action and 
+		 * parameters.
+		 *
+	 	 * The general pattern for the URI routing in this case is fairly common
+		 * Assumed default names are Index and index for controller and action,
+		 * though these can be changed using the methods.
+		 * 
+		 * Default routes:
+		 * #1 / (default controller, default action, no params)
+		 * #2 /<1> (<1> controller, default action, no params)
+		 * #3 /<1>/<2> (<1> controller, <2> action, no params)
+		 * #4 /<1>/<2>/<3> (<1> controller, default action, {<2>: <3>} params)
+		 * #5 /<1>/<2>/<3>/<4> (<1> controller, <2> action, {<3>: <4>} params)
+		 * Follows #4 and #5 based on whether number of pieces is even or odd (
+		 * odd = #4, even = #5)
+		 *
+		 * @param	string				The Uri to parse
+		 * @return	void				No value is returned
 		 */
-		public function parse($uri) {
-			// Explode the array into parts and clean it (remove empty values 
-			// "//")
-			$uri = \trim($uri, "/");
-			$parts = \explode("/", $uri);
-			foreach ($parts as $key => $value) {
-				if (empty($value)) {
+		public function parse($uri)
+		{
+			$parts = \explode('/', \trim($uri, '/'));
+
+			foreach(parts as $key => $value)
+			{
+				if(empty($value))
+				{
 					unset($parts[$key]);
 				}
 			}
-		
-			/**
-			 * Route based on the rules above - on the number of parts
-			 *
-	 		 * The general pattern for the URI routing in this case is fairly common
-			 * Assumed default names are Index and index for controller and action,
-			 * though these can be changed using the methods.
-			 * 
-			 * Default routes:
-			 * #1 / (default controller, default action, no params)
-			 * #2 /<1> (<1> controller, default action, no params)
-			 * #3 /<1>/<2> (<1> controller, <2> action, no params)
-			 * #4 /<1>/<2>/<3> (<1> controller, default action, {<2>: <3>} params)
-			 * #5 /<1>/<2>/<3>/<4> (<1> controller, <2> action, {<3>: <4>} params)
-			 * Follows #4 and #5 based on whether number of pieces is even or odd (
-			 * odd = #4, even = #5)
-			 */
-	
-			$controller = self::$defaultController;
-			$action = self::$defaultAction;
-			$params = array();
 
-			switch (count($parts)) {
-				case 1:
-					$controller = $parts[0];
-					break;
-				case 2:
-					$controller = $parts[0];
-					$action = $parts[1];
-					break;
-				default:	
-					// Use defaults if we have no parts
-					if (\count($parts) == 0) {
+			switch($parts_len = \sizeof($parts))
+			{
+				case(1):
+				{
+					$controller 	= $parts[0];
+				}
+				break;
+				case(2):
+				{
+					$controller	= $parts[0];
+					$action		= $parts[1];
+				}
+				break;
+				default:
+				{
+					if(!$parts_len)
+					{
 						break;
 					}
-				
-					// If the number of parts is even, use rule #5 otherwise rule #4
-					if (\count($parts) % 2 == 0) {
-						$controller = \array_shift($parts);
-						$action = \array_shift($parts);
-					} else {
-						$controller = \array_shift($parts);
+
+					$controller = \array_shift($parts);
+					--$parts_len;
+
+					if($parts_len || !($parts_len % 2))
+					{
+						$action	= \array_shift($parts);
+						--$parts_len;
 					}
-				
-					// Loop through the rest of the parts to collect parameters
-					for ($i = 0; $i < \count($parts); $i += 2) {
-						$params[$parts[$i]] = $parts[$i+1]; 
+
+					if($parts_len)
+					{
+						for($i = 0; $i < $parts_len; $i += 2)
+						{
+							$parameters[$parts[$i]] = (isset($parts[$i + 1]) ? $parts[$i + 1] : NULL);
+						}
 					}
-					break;
+				}
+				break;
 			}
-		
-			// Normalise and set the controller and action names
-			$this->setController($controller);
-			$this->setAction($action);
-			$this->params = $params;
+
+			$this->controller 	= (isset($controller) ? $controller : self::$default_controller);
+			$this->action		= (isset($action) ? $action : self::$default_action);
+			$this->parameters	= (isset($parameters) ? $parameters : Array());
 		}
 	}
 ?>
