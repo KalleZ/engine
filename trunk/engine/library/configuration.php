6aac3077190e190449b45fcea68753920fef95e3
kalle.php <kalle.php@cd68e5fc-efee-11de-91ba-91c70a792394>
2010-09-12 14:33:16 +0000

    Back to dev

--- trunk/engine/library/configuration.php
+++ trunk/engine/library/configuration.php
@@ -55,7 +55,7 @@
 	 * and some error messages will become more verbose. Fatal errors 
 	 * will display a backtrace
 	 */
-	'debug'		=> false, 
+	'debug'		=> true, 
 
 	/**
 	 * End application configuration
