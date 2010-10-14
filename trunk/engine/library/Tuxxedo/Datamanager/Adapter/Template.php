4a6a6e1bb9a24cf062f4338c3823312781025fbf
kalle.php <kalle.php@cd68e5fc-efee-11de-91ba-91c70a792394>
2010-10-14 17:28:38 +0000

    * Added template ids to the styles table * Added the template datamanager, with some work this will hopefully make the style datamanager work again

---
+++ trunk/engine/library/Tuxxedo/Datamanager/Adapter/Template.php
@@ -0,0 +1,163 @@
+<?php
+	/**
+	 * Tuxxedo Software Engine
+	 * =============================================================================
+	 *
+	 * @author		Kalle Sommer Nielsen 	<kalle@tuxxedo.net>
+	 * @author		Ross Masters 		<ross@tuxxedo.net>
+	 * @version		1.0
+	 * @copyright		Tuxxedo Software Development 2006+
+	 * @license		Apache License, Version 2.0
+	 * @package		Engine
+	 * @subpackage		Library
+	 *
+	 * =============================================================================
+	 */
+
+
+	/**
+	 * Datamanagers adapter namespace, this contains all the different 
+	 * datamanager handler implementations to comply with the standard 
+	 * adapter interface, and with the plugins for hooks.
+	 *
+	 * @author		Kalle Sommer Nielsen	<kalle@tuxxedo.net>
+	 * @author		Ross Masters 		<ross@tuxxedo.net>
+	 * @version		1.0
+	 * @package		Engine
+	 * @subpackage		Library
+	 */
+	namespace Tuxxedo\Datamanager\Adapter;
+
+
+	/**
+	 * Aliasing rules
+	 */
+	use Tuxxedo\Datamanager\Adapter;
+	use Tuxxedo\Exception;
+	use Tuxxedo\Registry;
+
+	/**
+	 * Include check
+	 */
+	defined('TUXXEDO_LIBRARY') or exit;
+
+
+	/**
+	 * Datamanager for templates
+	 *
+	 * @author		Kalle Sommer Nielsen <kalle@tuxxedo.net>
+	 * @version		1.0
+	 * @package		Engine
+	 * @subpackage		Library
+	 */
+	class Template extends Adapter
+	{
+		/**
+		 * Fields for validation of styles
+		 *
+		 * @var		array
+		 */
+		protected $fields		= Array(
+							'id'			=> Array(
+												'type'		=> self::FIELD_PROTECTED
+												), 
+							'title'			=> Array(
+												'type'		=> self::FIELD_REQUIRED, 
+												'validation'	=> self::VALIDATE_STRING
+												), 
+							'source'		=> Array(
+												'type'		=> self::FIELD_REQUIRED, 
+												'validation'	=> self::VALIDATE_STRING
+												), 
+							'compiledsource' 	=> Array(
+												'type'		=> self::FIELD_REQUIRED, 
+												'validation'	=> self::VALIDATE_STRING
+												), 
+							'defaultsource'		=> Array(
+												'type'		=> self::FIELD_REQUIRED, 
+												'validation'	=> self::VALIDATE_STRING
+												), 
+							'styleid'		=> Array(
+												'type'		=> self::FIELD_REQUIRED, 
+												'validation'	=> self::VALIDATE_NUMERIC
+												), 
+							'changed'		=> Array(
+												'type'		=> self::FIELD_OPTIONAL, 
+												'validation'	=> self::VALIDATE_BOOLEAN, 
+												'default'	=> false
+												), 
+							'revision'		=> Array(
+												'type'		=> self::FIELD_OPTIONAL, 
+												'validation'	=> self::VALIDATE_NUMERIC, 
+												'default'	=> 0
+												)
+							);
+
+
+		/**
+		 * Constructor, fetches a new template based on its id if set
+		 *
+		 * @param	\Tuxxedo\Registry		The Registry reference
+		 * @param	integer				The template id
+		 * @param	integer				Additional options to apply on the datamanager
+		 *
+		 * @throws	\Tuxxedo\Exception\Basic	Throws an exception if the template id is set and it failed to load for some reason
+		 * @throws	\Tuxxedo\Exception\SQL		Throws a SQL exception if a database call fails
+		 */
+		public function __construct(Registry $registry, $identifier = NULL, $options = self::OPT_DEFAULT)
+		{
+			$this->dmname		= 'template';
+			$this->tablename	= \TUXXEDO_PREFIX . 'templates';
+			$this->idname		= 'id';
+
+			if($identifier !== NULL)
+			{
+				$template = $registry->db->query('
+									SELECT 
+										* 
+									FROM 
+										`' . \TUXXEDO_PREFIX . 'templates` 
+									WHERE 
+										`id` = %d
+									LIMIT 1', $identifier);
+
+				if(!$template || !$template->getNumRows())
+				{
+					throw new Exception\Exception('Invalid template id passed to datamanager');
+				}
+
+				$this->data 		= $template->fetchAssoc();
+				$this->identifier 	= $identifier;
+
+				$template->free();
+			}
+
+			parent::init($registry, $options);
+		}
+
+		/**
+		 * Updates the template ids in the styles table
+		 *
+		 * @param	array				A virtually populated array from the datamanager abstraction
+		 * @return	boolean				Returns true if the datastore was updated with success, otherwise false
+		 */
+		public function rebuild(Array $virtual)
+		{
+			$dm 	= Adapter::factory('style', $this->get('styleid'), 0);
+			$ids	= \array_flip(\explode(',', $dm->get('templateids')));
+
+			if(!$virtual && isset($ids[$this->data['id']]))
+			{
+				unset($ids[$this->data['id']]);
+			}
+			elseif($virtual && !isset($ids[$this->data['id']]))
+			{
+				$ids[] = $this->data[$ids];
+			}
+
+			$dm['templateids'] = $ids;
+
+			return($dm->save());
+		}
+	}
+?>
