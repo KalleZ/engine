6034613d2c49218226ddb8b9d3add00b0c78c264
Kalle Sommer Nielsen <kalle@php.net>
2011-08-10 20:03:03 +0000

    We shouldn't load only here

--- dev/tools/styles.php
+++ dev/tools/styles.php
@@ -295,7 +295,7 @@
 				{
 					try
 					{
-						$dm 	= Datamanager\Adapter::factory('template', $input->get('id'), Datamanager\Adapter::OPT_LOAD_ONLY);
+						$dm 	= Datamanager\Adapter::factory('template', $input->get('id'));
 						$source	= htmlspecialchars($dm['source'], ENT_QUOTES, 'UTF-8');
 					}
 					catch(Exception $e)
