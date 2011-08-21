f50fc806880306171dd388effe7a8f7c050190f5
Kalle Sommer Nielsen <kalle@php.net>
2011-08-21 01:21:03 +0000

    Core: * Added iteratable interface into the Design namespace * Updated API dumps

---
+++ library/Tuxxedo/Design/Iteratable.php
@@ -0,0 +1,51 @@
+<?php
+	/**
+	 * Tuxxedo Software Engine
+	 * =============================================================================
+	 *
+	 * @author		Kalle Sommer Nielsen 	<kalle@tuxxedo.net>
+	 * @author		Ross Masters 		<ross@tuxxedo.net>
+	 * @version		1.0
+	 * @copyright		Tuxxedo Software Development 2006+
+	 * @license		Apache License, Version 2.0
+	 * @package		Engine
+	 * @subpackage		Library
+	 *
+	 * =============================================================================
+	 */
+
+
+	/**
+	 * Design namespace. This namespace is meant for abstract concepts and 
+	 * in most cases simply just interfaces that in someway structures the 
+	 * general design used in the core components.
+	 *
+	 * @author		Kalle Sommer Nielsen	<kalle@tuxxedo.net>
+	 * @author		Ross Masters 		<ross@tuxxedo.net>
+	 * @version		1.0
+	 * @package		Engine
+	 * @subpackage		Library
+	 */
+	namespace Tuxxedo\Design;
+
+
+	/**
+	 * Include check
+	 */
+	defined('\TUXXEDO_LIBRARY') or exit;
+
+
+	/**
+	 * Iteratable interface, this interface is simply just a 
+	 * combination of the built-in interfaces in PHP, Iterator 
+	 * and Countable
+	 *
+	 * @author		Kalle Sommer Nielsen <kalle@tuxxedo.net>
+	 * @version		1.0
+	 * @package		Engine
+	 * @subpackage		Library
+	 */
+	abstract class Iteratable implements \Iterator, \Countable
+	{
+	}
+?>
