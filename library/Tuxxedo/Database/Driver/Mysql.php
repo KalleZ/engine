a62f4920f7fa6ca11edaedf1b91bb10bca040838
Kalle Sommer Nielsen <kalle@php.net>
2012-10-26 17:48:45 +0000

    Resolve to the global namespace

--- library/Tuxxedo/Database/Driver/Mysql.php
+++ library/Tuxxedo/Database/Driver/Mysql.php
@@ -153,7 +153,7 @@
 
 			Registry::globals('error_reporting', false);
 
-			if(($link = $connect_function($hostname, $this->configuration['username'], $this->configuration['password'], false, ($this->configuration['ssl'] ? MYSQL_CLIENT_SSL : 0))) === false || !mysql_select_db($this->configuration['database'], $link))
+			if(($link = $connect_function($hostname, $this->configuration['username'], $this->configuration['password'], false, ($this->configuration['ssl'] ? MYSQL_CLIENT_SSL : 0))) === false || !\mysql_select_db($this->configuration['database'], $link))
 			{
 				Registry::globals('error_reporting', true);
 
