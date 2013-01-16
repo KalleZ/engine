356ba36a83f4294b73120beba7104401a9865478
Kalle Sommer Nielsen <kalle@php.net>
2013-01-16 13:25:44 +0000

    Change the codename to 'Invincible', instead of 'Timewarp', also revision 800!

--- library/Tuxxedo/Version.php
+++ library/Tuxxedo/Version.php
@@ -127,7 +127,7 @@
 		 *
 		 * @var		string
 		 */
-		const CODENAME			= 'Timewarp';
+		const CODENAME			= 'Invincible';
 
 		/**
 		 * Current version control system revision number
@@ -147,6 +147,6 @@
 		 *
 		 * @var		string
 		 */
-		const FULL			= '1.2.0 "Timewarp" Alpha 1 (experimental)';
+		const FULL			= '1.2.0 "Invincible" Alpha 1 (experimental)';
 	}
 ?>
