882f37b52d3d7f72f3b7e91c1f1c633bb49a5654
kalle.php <kalle.php@cd68e5fc-efee-11de-91ba-91c70a792394>
2013-05-11 13:37:45 +0000

    And actually use svn add too

---
+++ trunk/engine/library/Tuxxedo/Datamanager/Adapter/Phrasegroup.php
@@ -0,0 +1,216 @@
+<?php
+	/**
+	 * Tuxxedo Software Engine
+	 * =============================================================================
+	 *
+	 * @author		Kalle Sommer Nielsen 	<kalle@tuxxedo.net>
+	 * @author		Ross Masters 		<ross@tuxxedo.net>
+	 * @version		1.0
+	 * @copyright		Tuxxedo Software Development 2006+
+	 * @license		Apache License, Version 2.0
+	 * @package		Engine
+	 * @subpackage		Library
+	 *
+	 * =============================================================================
+	 */
+
+
+	/**
+	 * Datamanagers adapter namespace, this contains all the different 
+	 * datamanager handler implementations to comply with the standard 
+	 * adapter interface, and with the plugins for hooks.
+	 *
+	 * @author		Kalle Sommer Nielsen	<kalle@tuxxedo.net>
+	 * @author		Ross Masters 		<ross@tuxxedo.net>
+	 * @version		1.0
+	 * @package		Engine
+	 * @subpackage		Library
+	 */
+	namespace Tuxxedo\Datamanager\Adapter;
+
+
+	/**
+	 * Aliasing rules
+	 */
+	use Tuxxedo\Datamanager\Adapter;
+	use Tuxxedo\Datamanager\Hooks;
+	use Tuxxedo\Exception;
+	use Tuxxedo\Registry;
+
+
+	/**
+	 * Include check
+	 */
+	\defined('\TUXXEDO_LIBRARY') or exit;
+
+
+	/**
+	 * Datamanager for phrase groups
+	 *
+	 * @author		Kalle Sommer Nielsen <kalle@tuxxedo.net>
+	 * @version		1.0
+	 * @package		Engine
+	 * @subpackage		Library
+	 */
+	class Phrasegroup extends Adapter implements Hooks\Cache
+	{
+		/**
+		 * Fields for validation of phrase groups
+		 *
+		 * @var		array
+		 */
+		protected $fields		= Array(
+							'id'		=> Array(
+											'type'		=> self::FIELD_PROTECTED, 
+											'validation'	=> self::VALIDATE_IDENTIFIER
+											), 
+							'title'		=> Array(
+											'type'		=> self::FIELD_REQUIRED, 
+											'validation'	=> self::VALIDATE_STRING
+											), 
+							'language'	=> Array(
+											'type'		=> self::FIELD_REQUIRED, 
+											'validation'	=> self::VALIDATE_CALLBACK, 
+											'callback'	=> Array(__CLASS__, 'isValidLanguageId')
+											)
+							);
+
+
+		/**
+		 * Constructor, fetches a new phrase group based on its title if set
+		 *
+		 * @param	\Tuxxedo\Registry		The Registry reference
+		 * @param	integer				The phrase group title
+		 * @param	integer				Additional options to apply on the datamanager
+		 * @param	\Tuxxedo\Datamanager\Adapter	The parent datamanager if any
+		 *
+		 * @throws	\Tuxxedo\Exception\Basic	Throws an exception if the phrase group title is set and it failed to load for some reason
+		 * @throws	\Tuxxedo\Exception\SQL		Throws a SQL exception if a database call fails
+		 */
+		public function __construct(Registry $registry, $identifier = NULL, $options = self::OPT_DEFAULT, Adapter $parent = NULL)
+		{
+			$this->dmname		= 'phrasegroup';
+			$this->tablename	= \TUXXEDO_PREFIX . 'phrasegroups';
+			$this->idname		= 'id';
+
+			if($identifier !== NULL)
+			{
+				$phrasegroup = $registry->db->equery('
+									SELECT 
+										* 
+									FROM 
+										`' . \TUXXEDO_PREFIX . 'phrasegroups` 
+									WHERE 
+										`title` = \'%s\'', $identifier);
+
+				if(!$phrasegroup || !$phrasegroup->getNumRows())
+				{
+					throw new Exception('Invalid phrase group title passed to datamanager');
+
+					return;
+				}
+
+				$this->data 		= $phrasegroup->fetchAssoc();
+				$this->identifier 	= $identifier;
+
+				$phrasegroup->free();
+			}
+
+			parent::init($registry, $options, $parent);
+		}
+
+		/**
+		 * Checks whether a language id is valid or not
+		 *
+		 * @param	\Tuxxedo\Datamanager\Adapter	The current datamanager adapter
+		 * @param	\Tuxxedo\Registry		The Registry reference
+		 * @param	integer				The language id
+		 * @return	boolean				True if the language exists, otherwise false
+		 */
+		public static function isValidLanguageId(Adapter $dm, Registry $registry, $languageid = NULL)
+		{
+			return($languageid === NULL || $registry->datastore->languages && isset($registry->datastore->languages[$languageid]));
+		}
+
+		/**
+		 * Save the phrase group in the datastore, this method is called from 
+		 * the parent class in cases when the save method was success
+		 *
+		 * @return	boolean				Returns true if the datastore was updated with success, otherwise false
+		 */
+		public function rebuild()
+		{
+			if(($datastore = $this->registry->datastore->phrasegroups) === false)
+			{
+				$datastore = Array();
+			}
+
+			$id 		= (isset($this->data['title']) ? $this->data['title'] : $this->identifier);
+			$old_group	= '';
+			$new_group	= '';
+
+			if($this->context == self::CONTEXT_SAVE)
+			{
+				if(isset($this->data['title']))
+				{
+					foreach($datastore as $key => $value)
+					{
+						if($value == $id)
+						{
+							unset($datastore[$key]);
+
+							break;
+						}
+					}
+
+					$old_group = $id;
+				}
+
+				$datastore[] = $new_group = $this->data['title'];
+			}
+			elseif($this->context == self::CONTEXT_DELETE && \in_array($id, $datastore))
+			{
+				$new_group = '';
+				$old_group = $id;
+
+				foreach($datastore as $key => $value)
+				{
+					if($value == $id)
+					{
+						unset($datastore[$key]);
+
+						break;
+					}
+				}
+			}
+			else
+			{
+				return(false);
+			}
+
+			if(!empty($old_group))
+			{
+				$groups = $this->registry->db->equery('
+									SELECT 
+										`option` 
+									FROM 
+										`' . \TUXXEDO_PREFIX . 'phrases` 
+									WHERE 
+										`phrasegroup` = \'%s\'', $old_group);
+
+				if($groups && $groups->getNumRows())
+				{
+					foreach($groups as $row)
+					{
+						$dm 			= Adapter::factory('phrase', $row['title']);
+						$dm['phrasegroup']	= $new_group;
+
+						$dm->save();
+					}
+				}
+			}
+
+			return($this->registry->datastore->rebuild('phrasegroups', $datastore));
+		}
+	}
+?>
