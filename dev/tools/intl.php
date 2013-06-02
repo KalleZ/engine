e7f1523859c25c2fc4659a25b87bc3b9aaaf6fdf
Kalle Sommer Nielsen <kalle@php.net>
2013-06-02 08:17:39 +0000

    Enable the Internationalization navigation menu, finally!

---
+++ dev/tools/intl.php
@@ -0,0 +1,35 @@
+<?php
+	/**
+	 * Tuxxedo Software Engine
+	 * =============================================================================
+	 *
+	 * @author		Kalle Sommer Nielsen 	<kalle@tuxxedo.net>
+	 * @author		Ross Masters 		<ross@tuxxedo.net>
+	 * @version		1.0
+	 * @copyright		Tuxxedo Software Development 2006+
+	 * @license		Apache License, Version 2.0
+	 * @package		Engine
+	 * @subpackage		DevTools
+	 *
+	 * =============================================================================
+	 */
+
+
+	/**
+	 * Global templates
+	 */
+	$templates 		= Array(
+					);
+
+	/**
+	 * Set script name
+	 */
+	const SCRIPT_NAME	= 'intl';
+
+	/**
+	 * Require the bootstraper
+	 */
+	require('./includes/bootstrap.php');
+
+	tuxxedo_error('This is a WIP!');
+?>
