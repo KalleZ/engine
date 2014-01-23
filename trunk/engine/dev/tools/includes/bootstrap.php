800c15227d67031f1bf91ec33c43bf2e6655a448
kalle.php <kalle.php@cd68e5fc-efee-11de-91ba-91c70a792394>
2014-01-23 21:51:41 +0000

    * \Tuxxedo\LocalCache is no longer loaded when the database component is loaded (might be just temporary) * Changed \Tuxxedo\Bootstrap::setHook() to pass $configuration by reference as the 3rd parameter for callbacks * Removed all the clutter code in the DevTools bootstraper and implemented it using the new \Tuxxedo\Bootstrap code (it still needs a few fixes here and there)

--- trunk/engine/dev/tools/includes/bootstrap.php
+++ trunk/engine/dev/tools/includes/bootstrap.php
@@ -18,11 +18,8 @@
 	/**
 	 * Aliasing rules
 	 */
-	use DevTools\Style;
-	use Tuxxedo\Input;
-	use Tuxxedo\Registry;
+	use DevTools\Bootstrap;
 	use Tuxxedo\Template;
-	use Tuxxedo\Version;
 
 
 	/**
@@ -31,6 +28,7 @@
 	 * @var		string
 	 */
 	define('TUXXEDO_DIR', realpath('../../'));
+
 
 	/**
 	 * Sets the library path
@@ -41,88 +39,13 @@
 
 
 	require(TUXXEDO_LIBRARY . '/configuration.php');
-	require(TUXXEDO_LIBRARY . '/DevTools/functions.php');
-	require(TUXXEDO_LIBRARY . '/DevTools/functions_widget.php');
-	require(TUXXEDO_LIBRARY . '/Tuxxedo/Loader.php');
-	require(TUXXEDO_LIBRARY . '/Tuxxedo/functions.php');
+	require(TUXXEDO_LIBRARY . '/Tuxxedo/Bootstrap.php');
+	require(TUXXEDO_LIBRARY . '/DevTools/Bootstrap.php');
 
-	/**
-	 * Set database table prefix constant
-	 *
-	 * @var		string
-	 */
-	define('TUXXEDO_PREFIX', $configuration['database']['prefix']);
+	Bootstrap::setPreloadables('datastore', Array('languages', 'options', 'phrasegroups', 'usergroups'));
+	Bootstrap::setPreloadables('phrasegroups', Array('global'));
 
-	/**
-	 * SQLite uses relative paths
-	 */
-	$db_driver	= strtolower($configuration['database']['driver']);
-	$db_subdriver	= strtolower($configuration['database']['subdriver']);
-
-	if(($db_driver == 'sqlite' || ($db_driver == 'pdo' && $db_subdriver == 'sqlite')) && !empty($configuration['devtools']['database']))
-	{
-		$configuration['database']['database'] = $configuration['devtools']['database'];
-	}
-
-	unset($db_driver, $db_subdriver);
-
-	date_default_timezone_set('UTC');
-
-	tuxxedo_handler('exception', 'devtools_exception_handler');
-	tuxxedo_handler('error', 'tuxxedo_error_handler');
-	tuxxedo_handler('shutdown', 'tuxxedo_shutdown_handler');
-	tuxxedo_handler('autoload', '\Tuxxedo\Loader::load');
-
-	$registry = Registry::init($configuration);
-
-	Registry::globals('error_reporting', 	true);
-	Registry::globals('errors', 		Array());
-
-	$registry->set('timezone', new DateTimeZone('UTC'));
-	$registry->set('datetime', new DateTime('now', $timezone));
-
-	$configuration['application']['debug'] = false;
-
-	/**
-	 * Set the UTC time constant
-	 *
-	 * @var		integer
-	 */
-	define('TIMENOW_UTC', $datetime->getTimestamp());
-
-	if(!defined('SCRIPT_NAME'))
-	{
-		tuxxedo_doc_error('A script name must be defined prior to use');
-	}
-
-	$registry->register('db', '\Tuxxedo\Database');
-	$registry->register('datastore', '\Tuxxedo\Datastore');
-	$registry->register('cookie', '\Tuxxedo\Cookie');
-	$registry->register('session', '\DevTools\Session');
-
-	$registry->set('input', new Input);
-	$registry->set('style', new Style);
-
-	if(SCRIPT_NAME != 'datastore')
-	{
-		$cache_buffer		= Array();
-		$default_precache 	= Array('languages', 'options', 'phrasegroups', 'usergroups');
-
-		$datastore->cache((!isset($precache) ? $default_precache : array_unique(array_merge($default_precache, (array) $precache))), $cache_buffer) or tuxxedo_multi_error('Unable to load datastore elements', $cache_buffer);
-
-		$registry->register('options', '\Tuxxedo\Options');
-		$registry->register('intl', '\Tuxxedo\Intl');
-
-		$cache_buffer = Array();
-		$intl->cache(Array('global'), $cache_buffer) or tuxxedo_multi_error('Unable to load phrase groups', $cache_buffer);
-	}
-	else
-	{
-		$registry->register('options', '\Tuxxedo\Options');
-	}
-
-	$cache_buffer		= Array();
-	$default_templates 	= Array('header', 'footer', 'error', 'redirect', 'multierror', 'multierror_itembit');
+	$default_templates = Array('header', 'footer', 'error', 'redirect', 'multierror', 'multierror_itembit');
 
 	if($configuration['devtools']['protective'])
 	{
@@ -134,33 +57,26 @@
 		$default_templates = array_merge($default_templates, (array) $action_templates[(string) $_GET['do']]);
 	}
 
-	$style->cache((!isset($templates) ? $default_templates : array_merge($default_templates, (array) $templates)), $cache_buffer) or tuxxedo_multi_error('Unable to load templates', $cache_buffer);
+	Bootstrap::setPreloadables('templates', (!isset($templates) ? $default_templates : array_merge($default_templates, (array) $templates)));
 
-	unset($cache_buffer);
+	unset($templates);
 
-	$engine_version = Version::FULL;
-	$widget_hook	= false;
 
-	if(($widget = $style->getSidebarWidget($widget_hook)) !== false)
+	/**
+	 * Bootstrap
+	 */
+	if(SCRIPT_NAME != 'datastore')
 	{
-		if(!$widget_hook)
-		{
-			eval('$widget = "' . $widget . '";');
-		}
+		Bootstrap::init(Bootstrap::MODE_CUSTOM, Bootstrap::FLAG_DATE | Bootstrap::FLAG_DATABASE | Bootstrap::FLAG_DATASTORE | Bootstrap::FLAG_OPTIONS | Bootstrap::FLAG_INTL);
+	}
+	else
+	{
+		Bootstrap::init(Bootstrap::MODE_CUSTOM, Bootstrap::FLAG_DATE | Bootstrap::FLAG_DATABASE | Bootstrap::FLAG_DATASTORE | Bootstrap::FLAG_OPTIONS);
 	}
 
-	unset($widget_hook);
-
-	Template::globalSet('widget', $widget);
-	Template::globalSet('engine_version', $engine_version);
-
-	eval('$header = "' . $style->fetch('header') . '";');
-	eval('$footer = "' . $style->fetch('footer') . '";');
-
-	if($configuration['devtools']['protective'])
-	{
-		devtools_auth_handler();
-	}
-
-	$configuration['application']['debug'] = true;
+	/**
+	 * Header and footer templates for the main site
+	 */
+	$header = new Template('header', true);
+	$footer = new Template('footer', true);
 ?>
---
+++ trunk/engine/library/DevTools/Bootstrap.php
@@ -0,0 +1,140 @@
+<?php
+	/**
+	 * Tuxxedo Software Engine
+	 * =============================================================================
+	 *
+	 * @author		Kalle Sommer Nielsen 	<kalle@tuxxedo.net>
+	 * @author		Ross Masters 		<ross@tuxxedo.net>
+	 * @version		1.0
+	 * @copyright		Tuxxedo Software Development 2006+
+	 * @license		Apache License, Version 2.0
+	 * @package		Engine
+	 * @subpackage		DevTools
+	 *
+	 * =============================================================================
+	 */
+
+
+	/**
+	 * Developmental Tools namespace. This namespace is for all development 
+	 * tool related routines, as used by /dev/tools.
+	 *
+	 * @author              Kalle Sommer Nielsen <kalle@tuxxedo.net>
+	 * @version             1.0
+	 * @package             Engine
+	 * @subpackage          DevTools
+	 */
+	namespace DevTools;
+
+
+	/**
+	 * Aliasing rules
+	 */
+	use DevTools\Style;
+	use Tuxxedo\Input;
+	use Tuxxedo\Registry;
+	use Tuxxedo\Template;
+	use Tuxxedo\Version;
+
+
+	/**
+	 * Include check
+	 */
+	\defined('\TUXXEDO_LIBRARY') or exit;
+
+
+	/**
+	 * Development Tools bootstraper, this integrates itself with the 
+	 * global bootstraper by using its hooks and other custom overrides 
+	 * thats needed specifically for the DevTools.
+	 *
+	 * @author		Kalle Sommer Nielsen <kalle@tuxxedo.net>
+	 * @version		1.0
+	 * @package		Engine
+	 * @subpackage		DevTools
+	 * @since		1.2.0
+	 *
+	 * @wip
+	 */
+	class Bootstrap extends \Tuxxedo\Bootstrap
+	{
+		/**
+		 * Pre initializer
+		 *
+		 * @param	integer				The flags passed to the bootstraper initializer method
+		 * @return	void				No value is returned
+		 */
+		public static function preInit($flags)
+		{
+			require(TUXXEDO_LIBRARY . '/DevTools/functions.php');
+			require(TUXXEDO_LIBRARY . '/DevTools/functions_widget.php');
+
+			parent::setHook(parent::FLAG_CORE, function(Registry $registry, Array $preloadables = NULL, &$configuration = NULL)
+			{
+				if(!\defined('SCRIPT_NAME'))
+				{
+					\tuxxedo_doc_error('A script name must be defined prior to use');
+				}
+
+				\tuxxedo_handler('exception', '\devtools_exception_handler');
+
+				$registry->set('style', new Style);
+
+				if($preloadables)
+				{
+					$cache_buffer = Array();
+
+					$registry->style->cache($preloadables, $cache_buffer) or \tuxxedo_multi_error('Unable to load templates', $cache_buffer);
+
+					unset($cache_buffer);
+
+					self::$preloadables['templates'] = Array();
+				}
+			}, 'templates');
+
+			parent::setHook(parent::FLAG_DATABASE, function(Registry $registry, Array $preloadables = NULL, &$configuration = NULL)
+			{
+				$db_driver	= strtolower($configuration['database']['driver']);
+				$db_subdriver	= strtolower($configuration['database']['subdriver']);
+
+				if(($db_driver == 'sqlite' || ($db_driver == 'pdo' && $db_subdriver == 'sqlite')) && !empty($configuration['devtools']['database']))
+				{
+					$configuration['database']['database'] = $configuration['devtools']['database'];
+				}
+			});
+		}
+
+		/**
+		 * Post initializer
+		 *
+		 * @param	\Tuxxedo\Registry		The Tuxxedo registry object
+		 * @return	void				No value is returned
+		 */
+		public static function postInit(Registry $registry)
+		{
+			$registry->register('cookie', '\Tuxxedo\Cookie');
+			$registry->register('session', '\DevTools\Session');
+			$registry->set('input', new Input);
+
+			$widget_hook	= false;
+
+			if(($widget = $registry->style->getSidebarWidget($widget_hook)) !== false)
+			{
+				if(!$widget_hook)
+				{
+					eval('$widget = "' . $widget . '";');
+				}
+			}
+
+			Template::globalSet('widget', $widget);
+			Template::globalSet('engine_version', Version::FULL);
+
+			$configuration = $registry->getConfiguration();
+
+			if($configuration['devtools']['protective'])
+			{
+				\devtools_auth_handler();
+			}
+		}
+	}
+?>
