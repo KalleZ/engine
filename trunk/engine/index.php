11203e99c8ee3b69cfe4a4257957cb4d3b231463
kalle.php <kalle.php@cd68e5fc-efee-11de-91ba-91c70a792394>
2014-03-06 02:40:22 +0000

    Remove this developmental code

--- trunk/engine/index.php
+++ trunk/engine/index.php
@@ -36,32 +36,7 @@
 	 */
 	require('./library/bootstrap.php');
 
-use Tuxxedo\Upload;
 
-$u = new Upload;
-
-if(isset($_POST['send']))
-{
-	$u->queue('post', 'fileselector1', 'image');
-	$u->queue('url', $_POST['fileselector2'], 'image');
-
-	$status = $u->upload();
-
-	echo '<pre>';
-	var_dump($status);
-	echo '</pre>';
-}
-
-?>
-<form enctype="multipart/form-data" action="<?php echo(htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES)); ?>" method="POST">
-    <input type="hidden" name="MAX_FILE_SIZE" value="<?php echo($u['size_limit']); ?>" />
-    Send this file: <input name="fileselector1" type="file" /> <br />
-    Send this file: <input name="fileselector2" type="text" /> <br />
-    <input type="submit" name="send" value="Send File" />
-</form>
-<?php
-
-//exit;
 	/**
 	 * Just print the engine version to show that
 	 * the bootstraper was a success
