5989413f09366f3dc0d53dafd0c735f2cf11c67f
kalle.php <kalle.php@cd68e5fc-efee-11de-91ba-91c70a792394>
2014-06-10 14:14:05 +0000

    * More work on the Expat parser * Fixed some consistency with the SimpleXML parser for when it populates the \Tuxxedo\Xml\Tree structure * Finished the new changelog page

--- trunk/engine/library/Tuxxedo/Xml/Parser/Expat.php
+++ trunk/engine/library/Tuxxedo/Xml/Parser/Expat.php
@@ -63,6 +63,18 @@
 		 */
 		protected static $parser;
 
+		/**
+		 * Holds the stack data used to be shared between 
+		 * the recursive parser calls
+		 *
+		 * @var		\stdClass
+		 */
+		protected $stack_data		= [
+							'depth'		=> 0, 
+							'tree'		=> NULL, 
+							'current'	=> NULL
+							];
+
 
 		/**
 		 * Constructs a new EXPAT parser object
@@ -78,9 +90,12 @@
 
 			if(!self::$parser)
 			{
-				self::$parser = xml_parser_create();
+				self::$parser = \xml_parser_create();
 
-				xml_set_element_handler(self::$parser, [$this, 'doParseStart'], [$this, 'doParseEnd']);
+				\xml_set_element_handler(self::$parser, [$this, 'doParseStart'], [$this, 'doParseEnd']);
+				\xml_set_character_data_handler(self::$parser, [$this, 'doCdata']);
+
+				\xml_parser_set_option(self::$parser, \XML_OPTION_CASE_FOLDING, false);
 			}
 		}
 
@@ -91,7 +106,7 @@
 		{
 			if(self::$parser)
 			{
-				xml_parser_free(self::$parser);
+				\xml_parser_free(self::$parser);
 			}
 		}
 
@@ -102,24 +117,80 @@
 		 * @reutrn	\Tuxxedo\Xml\Tree			Returns an XML tree of the parsed XML
 		 *
 		 * @throws	\Tuxxedo\Exception\Xml			Throws an XML exception if parsing fails for some reason
-		 *
-		 * @todo	Implement
 		 */
 		public function parse($input)
 		{
 			if(!self::$parser)
 			{
-				self::$parser = xml_parser_create();
+				self::$parser = \xml_parser_create();
 
-				xml_set_element_handler(self::$parser, [$this, 'doParseStart'], [$this, 'doParseEnd']);
+				\xml_set_element_handler(self::$parser, [$this, 'doParseStart'], [$this, 'doParseEnd']);
+				\xml_set_character_data_handler(self::$parser, [$this, 'doCdata']);
+
+				\xml_parser_set_option(self::$parser, \XML_OPTION_CASE_FOLDING, false);
 			}
 
-			if(!xml_parse(self::$parser, $input))
+			if(!\xml_parse(self::$parser, $input))
 			{
-				throw new Exception\Xml('XML', Exception\Xml::TYPE_EXPAT, self::$parser);
+				throw new Exception\Xml('XML-Expat', Exception\Xml::TYPE_EXPAT, self::$parser);
 			}
 
-			var_dump(self::$parser);
+			return($this->doStackToTree($this->stack_data));
+		}
+
+		/**
+		 * Handles the start of parsing a tag (creates the stack data for 
+		 * the current tag for use with the the character data parser)
+		 *
+		 * @param	resource				The XML parser resource
+		 * @param	string					The tag name
+		 * @param	array					Optionally the attributes in key value pairs
+		 * @return	void					No value is returned
+		 */
+		protected function doParseStart($parser, $name, Array $attr = [])
+		{
+			$this->stack_data['current']	= [
+								'name'	=> $name, 
+								'attr'	=> $attr, 
+								'value'	=> '', 
+								'nodes	=> []
+								];
+
+			++$this->stack_data['depth'];
+		}
+
+		/**
+		 * ...
+		 *
+		 * @todo	Implement
+		 * @todo	Docblock
+		 */
+		protected function doParseEnd($parser, $name)
+		{
+			printf('END TAG: %s DEPTH: %d<br>' . PHP_EOL, $name, $this->stack_data['depth']);
+
+			--$this->stack_data['depth'];
+		}
+
+		/**
+		 * Handles the character data (applies to the current stack data)
+		 *
+		 * @param	string					The current tag data (value)
+		 * @return	void					No value is returned
+		 */
+		protected function doCdata($parser, $data)
+		{
+			$this->stack_data['current']['value'] .= $data;
+		}
+
+		/**
+		 * ...
+		 *
+		 * @todo	Implement
+		 * @todo	Docblock
+		 */
+		protected function doStackToTree(Array &$stack_data)
+		{
 		}
 	}
 ?>
